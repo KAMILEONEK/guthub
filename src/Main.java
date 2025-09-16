01a973e4f23891722ee99dc468457471add1f610
kamil <costam@gmai.com>
2025-09-16 11:26:32 +0200

    hej

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public  class Main {
     public static  void main(String[] args) {
-        System.out.println("Dzien dobry");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swoje imie");
+        String name = scanner.nextLine();
+
+
+        System.out.println("Dzien dobry " + name);
         System.out.println("dzis nw ktory jest");
     }
 }
